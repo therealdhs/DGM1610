34dfd0d26ed3287690b68e808ab5938937a1b1e0
Hunter <hunter.shepardson@gmail.com>
2021-10-22 22:30:26 -0600

    Challenge 1 Completed
    Finished Challenge 1 - plane simulator

--- Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -21,8 +21,10 @@ public class PlayerController : MonoBehaviour
         //Move the vehicle forward
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
+
         //Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
         //Rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
 
