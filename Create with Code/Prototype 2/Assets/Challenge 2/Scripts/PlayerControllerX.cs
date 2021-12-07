10b9906e9e6696fc88b1c46b4957ed524afe0928
Hunter <hunter.shepardson@gmail.com>
2021-12-06 23:59:39 -0700

    Challenge 2 Completed
    Play fetch with dog

---
+++ Create with Code/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float coolDown = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) & coolDown > 0.8f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            coolDown = 0.0f;
+        }
+        coolDown = coolDown + Time.deltaTime;
+    }
+}
